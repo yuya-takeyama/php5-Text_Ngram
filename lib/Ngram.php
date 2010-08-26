06881450fc1ce6177c6bce38253190a2a34364d1
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2010-08-27 00:35:48 +0900

    1st commit.

---
+++ lib/Ngram.php
@@ -0,0 +1,251 @@
+<?php
+/**
+ * N-gram 転置インデックス
+ *
+ * @author Yuya Takeyama
+ */
+class Ngram implements SeekableIterator, Countable, ArrayAccess
+{
+    /**
+     * N-gram の元になる文書
+     *
+     * @var string
+     */
+    protected $_text;
+
+    /**
+     * 文書の文字数
+     *
+     * @var int
+     */
+    protected $_textSize;
+
+    /**
+     * N-gram インデックスの長さ
+     *
+     * @var int
+     */
+    protected $_indexSize;
+
+    /**
+     * N-gram の元になる文書の文字コード
+     *
+     * @var string
+     */
+    protected $_encode;
+
+    /**
+     * N-gram イテレータのポインタ
+     *
+     * @var int
+     */
+    protected $_pointer;
+
+    /**
+     * Constructor
+     *
+     * @param  string $text   N-gram の元になる文書
+     * @param  int    $size   N-gram インデックスの長さ (デフォルトは 2 [bigram])
+     * @param  string $encode 文書の文字コード (省略時は mb_internal_encoding() を使用)
+     */
+    public function __construct($text, $size = 2, $encode = NULL)
+    {
+        $this->_text      = (string) $text;
+        $this->_indexSize = (int) $size;
+        $this->_encode    = isset($encode) ? $encode : mb_internal_encoding();
+        $this->_textSize  = mb_strlen($text, $this->_encode);
+        $this->rewind();
+    }
+
+    /**
+     * 配列として出力
+     *
+     * @param  void
+     * @return array N-gram の配列
+     */
+    public function toArray()
+    {
+        $result = array();
+        foreach ($this as $index)
+        {
+            $result[] = $index;
+        }
+        return $result;
+    }
+
+    /**
+     * イテレータのポインタの初期化
+     *
+     * @param  void
+     * @return void
+     * @see    Iterator
+     */
+    public function rewind()
+    {
+        $this->_pointer = 0;
+    }
+
+    /**
+     * イテレータの現在のポインタの取得
+     *
+     * @param  void
+     * @return int
+     * @see    Iterator
+     */
+    public function key()
+    {
+        return $this->_pointer;
+    }
+
+    /**
+     * イテレータの現在の要素の取得
+     *
+     * @param  void
+     * @return mixed
+     * @see    Iterator
+     */
+    public function current()
+    {
+        return $this->_getFromOffset($this->_pointer);
+    }
+
+    /**
+     * イテレータのポインタを次に進める
+     *
+     * @param  void
+     * @return void
+     * @see    Iterator
+     */
+    public function next()
+    {
+        $this->_pointer++;
+    }
+
+    /**
+     * イテレータに次の要素があるか
+     *
+     * @param  void
+     * @return bool 次の要素があれば true
+     */
+    public function valid()
+    {
+        return $this->_pointer < $this->_textSize - ($this->_indexSize - 1);
+    }
+
+    /**
+     * イテレータのポインタを $offset 番目に移動する
+     *
+     * @param  int  $offset
+     * @return void
+     * @throws OutOfBoundsException
+     */
+    public function seek($offset)
+    {
+        if (isset($this[$offset])) {
+            $this->_pointer = $offset;
+        } else {
+            throw new OutOfBoundsException("The offset '{$offset}' doesn't exist on the object.");
+        }
+    }
+
+    /**
+     * N-gram インデックスの要素数
+     *
+     * @param  void
+     * @return int
+     * @see    Countable
+     */
+    public function count()
+    {
+        return $this->_textSize - ($this->_indexSize - 1);
+    }
+
+    /**
+     * N-gram インデックスに $offset 番目の要素が存在するか
+     *
+     * @param  int  $offset
+     * @return bool 要素が存在すれば true
+     * @see    ArrayAccess
+     */
+    public function offsetExists($offset)
+    {
+        $offset = (int) $offset;
+        return 0 <= $offset && $offset < $this->count();
+    }
+
+    /**
+     * N-gram インデックスから $offset 番目の要素を取得
+     * $ngram[$offset] という形式で、配列のようにアクセスできる
+     *
+     * @param  int    $offset
+     * @return string
+     * @see    ArrayAccess
+     */
+    public function offsetGet($offset)
+    {
+        $offset = (int) $offset;
+        return $this->_getFromOffset($offset);
+    }
+
+    /**
+     * ArrayAccess interface のセッターメソッド
+     * ただし、Ngram はイミュータブルなので、使用不可
+     *
+     * @param  $offset
+     * @param  $val
+     * @return void
+     * @throws BadMethodCallException
+     * @see    ArrayAccess
+     */
+    public function offsetSet($offset, $val)
+    {
+        throw new BadMethodCallException('Ngram object is immutable.');
+    }
+
+    /**
+     * ArrayAccess interface の要素削除メソッド
+     * ただし、Ngram はイミュータブルなので、使用不可
+     *
+     * @param  $offset
+     * @return void
+     * @throws BadMethodCallException
+     * @see    ArrayAccess
+     */
+    public function offsetUnset($offset)
+    {
+        throw new BadMethodCallException('Ngram object is immutable.');
+    }
+
+    /**
+     * インデックスの長さを取得
+     *
+     * @param  void
+     * @return int
+     */
+    public function getIndexSize()
+    {
+        return $this->_indexSize;
+    }
+
+    /**
+     * 文書の文字数を取得
+     *
+     * @param  void
+     * @return int
+     */
+    public function getTextSize()
+    {
+        return $this->_textSize;
+    }
+
+    /**
+     * 指定した位置からインデックスを取得
+     *
+     * @param  int    $offset
+     * @return string 転地インデックスの一要素
+     */
+    private function _getFromOffset($offset)
+    {
+        return mb_substr($this->_text, $offset, $this->_indexSize, $this->_encode);
+    }
+}
---
+++ test/NgramTest.php
@@ -0,0 +1,132 @@
+<?php
+require_once 'PHPUnit/Framework.php';
+
+require_once dirname(__FILE__) . '/../lib/Ngram.php';
+
+/**
+ * Test class for Ngram.
+ */
+class NgramTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Ngram
+     */
+    protected $ngram;
+
+    protected function setUp()
+    {
+        $this->bigram  = new Ngram('こんにちは世界！', 2, 'UTF-8');
+        $this->trigram = new Ngram('こんにちは世界！', 3, 'UTF-8');
+    }
+
+    public function testToArray()
+    {
+        $expectedBigram = array(
+            'こん',
+            'んに',
+            'にち',
+            'ちは',
+            'は世',
+            '世界',
+            '界！'
+        );
+        $expectedTrigram = array(
+            'こんに',
+            'んにち',
+            'にちは',
+            'ちは世',
+            'は世界',
+            '世界！'
+        );
+        $this->assertEquals($expectedBigram, $this->bigram->toArray());
+        $this->assertEquals($expectedTrigram, $this->trigram->toArray());
+    }
+
+    public function testRewind()
+    {
+        $this->assertEquals(0, $this->bigram->key());
+        $this->bigram->next();
+        $this->assertEquals(1, $this->bigram->key());
+        $this->bigram->rewind();
+        $this->assertEquals(0, $this->bigram->key());
+    }
+
+    public function testCurrent()
+    {
+        $this->assertEquals('こん', $this->bigram->current());
+        $this->bigram->next();
+        $this->assertEquals('んに', $this->bigram->current());
+    }
+
+    public function testValid()
+    {
+        $this->bigram->seek(6);
+        $this->assertTrue($this->bigram->valid());
+        $this->bigram->next();
+        $this->assertFalse($this->bigram->valid());
+    }
+
+    public function testSeek()
+    {
+        $expected = 'んに';
+        $this->bigram->seek(1);
+        $this->assertEquals($expected, $this->bigram->current());
+    }
+
+    /**
+     * @expectedException OutOfBoundsException
+     */
+    public function testSeekThrowsOutOfBoundsException()
+    {
+        $this->bigram->seek(7);
+    }
+
+    public function testCount()
+    {
+        $this->assertEquals(7, count($this->bigram));
+        $this->assertEquals(6, count($this->trigram));
+    }
+
+    public function testOffsetExists()
+    {
+        $this->assertTrue($this->bigram->offsetExists(6));
+        $this->assertTrue(isset($this->bigram[6]));
+        $this->assertFalse($this->bigram->offsetExists(7));
+        $this->assertFalse(isset($this->bigram[7]));
+    }
+
+    public function testOffsetGet()
+    {
+        $expected = 'こん';
+        $this->assertEquals($expected, $this->bigram->offsetGet(0));
+        $this->assertEquals($expected, $this->bigram[0]);
+    }
+
+    /**
+     * @expectedException BadMethodCallException
+     */
+    public function testOffsetSet()
+    {
+        $this->bigram[0] = 'foo';
+    }
+
+    /**
+     * @expectedException BadMethodCallException
+     */
+    public function testOffsetUnset()
+    {
+        unset($this->bigram[0]);
+    }
+
+    public function testGetIndexSize()
+    {
+        $this->assertEquals(2, $this->bigram->getIndexSize());
+        $this->assertEquals(3, $this->trigram->getIndexSize());
+    }
+
+    public function testGetTextSize()
+    {
+        $this->assertEquals(8, $this->bigram->getTextSize());
+        $this->assertEquals(8, $this->trigram->getTextSize());
+    }
+}
